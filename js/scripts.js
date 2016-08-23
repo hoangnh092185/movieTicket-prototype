e261b756511482718b08c2abe0beb20a2546bb3f
Josh Huffman and Nhat Hoang <nhat@>
2016-08-23 11:14:40 -0700

    add functionality to select movie tickets and get a total price

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,48 @@
-function Ticket(movieTitle, threeD, matinee, ageRange) {
+function Ticket(movieTitle, threeD, matinee, ageRange, numberTickets) {
   this.movieTitle = movieTitle;
   this.threeD = threeD;
   this.matinee = matinee;
   this.ageRange = ageRange;
+  this.numberTickets = numberTickets;
 }
 
-function getPrice(ticket){
+Ticket.prototype.getPrice = function(){
   var ticketPrice = 12;
-  if (ticket.matinee){
+  if (this.matinee){
     ticketPrice -= 4;
   }
-  if (ticket.threeD){
+  if (this.threeD){
     ticketPrice += 3;
   }
-  if(ticket.ageRange === "senior" || ticket.ageRange === "student"){
+  if(this.ageRange === "senior" || this.ageRange === "student"){
     ticketPrice -=3;
   }
-  return ticketPrice;
+  return ticketPrice * this.numberTickets;
 }
-
 
 
 $(document).ready(function() {
-  var thisTicket = new Ticket("star wars", false, false, "senior");
-  console.log(thisTicket);
-  console.log(getPrice(thisTicket));
+  var ticketArray = [];
+  $("form.form-group").submit(function(event){
+    event.preventDefault();
+    var movieTitle = $("select.select-movie").val();
+    var ThreeD = ($("select.select-threeD").val() === "true");
+    var Matinee = ($("select.select-matinee").val() === "true");
+    var ageRange = $("select.select-ageRange").val();
+    var numberTickets = $("select.select-number").val();
+    console.log(movieTitle, ThreeD, Matinee, ageRange);
+
+    var thisTicket = new Ticket(movieTitle, ThreeD, Matinee, ageRange, numberTickets);
+    ticketArray.push(thisTicket);
+    console.log(thisTicket);
+    console.log(thisTicket.getPrice());
+
+    $("#ticketPrices").text("");
+    var price = 0;
+    ticketArray.forEach(function(ticket){
+      $("#ticketPrices").append("<li>" + ticket.movieTitle +" : $"+ ticket.getPrice()+ "   "+ ticket.ageRange +"   "+ ticket.numberTickets+"x"+ "</li>");
+      price += ticket.getPrice();
+    });
+    $("#totalPrice").text("Total cost: $"+price);
+  });
 });
